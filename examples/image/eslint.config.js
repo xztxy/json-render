3259c0ac85d7971c43997d802923c9374fc25871
Chris Tate <chris@ctate.dev>
2026-02-27 12:52:04 -0600

    fix ci

---
+++ examples/image/eslint.config.js
@@ -0,0 +1,11 @@
+import { nextJsConfig } from "@internal/eslint-config/next-js";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  ...nextJsConfig,
+  {
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
+];
