0716b1075c602a0e537e3bc79481bccbb16b4812
Chris Tate <chris@ctate.dev>
2026-03-02 15:52:48 -0600

    merge latest

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,10 +8,6 @@ export default defineConfig({
     alias: {
       react: path.resolve(__dirname, "node_modules/react"),
       "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
-      "@internal/react-state": path.resolve(
-        __dirname,
-        "packages/react-state/src/index.tsx",
-      ),
       vue: path.resolve(__dirname, "packages/vue/node_modules/vue"),
     },
   },
